30afd80b9d40e0275e1dc3a3c843bda9f2b566fb
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-02 01:53:56 -0800

    http server with routes

--- chapter1/server.js
+++ chapter1/server.js
@@ -1,10 +1,25 @@
 "use strict"
 const http = require('http');
+const path = require('path');
+let pages = [
+  {route: '', output: 'Wooho!'},
+  {route: 'about', output: 'A simple routing with Node exapmle'},
+  {route: 'another page', output: function() {return 'Here\s' + this.route;}}
+]
+
 const server = http.createServer();
 server.on('request', (req, res) => {
-  console.log(req.method);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Cool');
+  let lookup = path.basename(decodeURI(req.url));
+  pages.forEach((page) => {
+    if (page.route === lookup) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(typeof page.output === 'function' ? page.output() : page.output);
+    }
+  });
+  if (!res.finished) {
+    res.writeHead(404);
+    res.end('Page Not Found!');
+  }
 });
 
 server.listen(3002);
