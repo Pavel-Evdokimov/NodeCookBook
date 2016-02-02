c3ce33cb0978138ef3705272170ea736e435a4d9
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-02 02:14:42 -0800

    Parsing the querystring module

--- chapter1/server.js
+++ chapter1/server.js
@@ -1,21 +1,26 @@
 "use strict"
 const http = require('http');
-const path = require('path');
+const url = require('url');
+
 let pages = [
-  {route: '', output: 'Wooho!'},
-  {route: 'about', output: 'A simple routing with Node exapmle'},
-  {route: 'another page', output: function() {return 'Here\s' + this.route;}}
-]
+  {id: '1', route: '', output: 'Woohoo!'},
+  {id: '2', route: 'about', output: 'A simple routing with Node example'},
+  {id: '3', route: 'another page', output: function() {
+    return 'Here\'s ' + this.route;
+  }}
+];
 
 const server = http.createServer();
 server.on('request', (req, res) => {
-  let lookup = path.basename(decodeURI(req.url));
-  pages.forEach((page) => {
-    if (page.route === lookup) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(typeof page.output === 'function' ? page.output() : page.output);
-    }
-  });
+  let id = url.parse(decodeURI(req.url), true).query.id;
+  if (id) {
+    pages.forEach((page) => {
+      if (page.id === id) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(typeof page.output === 'function' ? page.output() : page.output);
+      }
+    });
+  }
   if (!res.finished) {
     res.writeHead(404);
     res.end('Page Not Found!');
