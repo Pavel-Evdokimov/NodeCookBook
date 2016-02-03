3470b32de270edc4c770ac6fa9f61b32457f99f0
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-03 17:40:30 +0300

    Uploading files via PUT

--- chapter2/server.js
+++ chapter2/server.js
@@ -1,10 +1,30 @@
 "use strict";
 const http = require('http');
 const formidable = require('formidable');
-const form = require('fs').readFileSync('chapter2/form.html');
+const fs = require('fs');
+const form = fs.readFileSync('chapter2/put_upload_form.html');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
+  if (req.method === "PUT") {
+    let fileData = new Buffer(+req.headers['content-length']);
+    let bufferOffset = 0;
+    req.on('data', (chunk) => {
+      chunk.copy(fileData, bufferOffset);
+      bufferOffset += chunk.length;
+    }).on('end', () => {
+      let rand = (Math.random()*Math.random()).toString(16).replace('.', '');
+      let to = 'chapter2/uploads/' + rand + "-" + req.headers['x-uploadedfilename'];
+
+      fs.writeFile(to, fileData, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log('Saved file to ' + to);
+        res.end();
+      });
+    });
+  }
   if (req.method === "GET") {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(form);
