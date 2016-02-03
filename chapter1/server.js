88dc63ff8652b4b287441e2af1ebe80358d71651
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-03 13:11:26 +0300

    Protecting against process memory overruns

--- chapter1/server.js
+++ chapter1/server.js
@@ -10,7 +10,24 @@ const mimeTypes = {
   '.css' : 'text/css'
 };
 
-let cache = {};
+let cache = {
+  store: {},
+  maxSize: 26214400, // (bytes) 25mb
+  maxAge: 5400 * 1000, //(ms) 1 and a half hours
+  cleanAfter: 7200 * 1000, //(ms) two hours
+  cleanedAt: 0, //to be set dynamically
+  clean: function(now) {
+    if (now - this.cleanAfter > this.cleanedAt) {
+      this.cleanedAt = now;
+      let that = this;
+      Object.keys(this.store).forEach(function(file) {
+        if (now > that.store[file].timestamp + that.maxAge) {
+          delete that.store[file];
+        }
+      });
+    }
+  }
+};
 
 const server = http.createServer();
 
@@ -39,15 +56,15 @@ server.on('request', (req, res) => {
         res.writeHead(500);
         res.end('Server Error!');
       });
-      s.on('data', (chunk) => {
-      });
       fs.stat(f, (err, stats) => {
-        let bufferOffset = 0;
-        cache[f] = {content: new Buffer(stats.size)};
-        s.on('data', (chunk) => {
-          chunk.copy(cache[f].content, bufferOffset);
-          bufferOffset += chunk.length;
-        });
+        if (stats.size < cache.maxSize) {
+          let bufferOffset = 0;
+          cache.store[f] = {content: new Buffer(stats.size), timestamp: Date.now()};
+          s.on('data', (data) => {
+            data.copy(cache.store[f].content, bufferOffset);
+            bufferOffset += data.length;
+          });
+        }
       });
       s.pipe(res);
       return;
@@ -55,6 +72,7 @@ server.on('request', (req, res) => {
     res.writeHead(404);
     res.end();
   });
+  cache.clean(Date.now());
 });
 
 server.listen(3002);
