2f7be60a3b8d7f0e7e77a8a80fb3887d795ab053
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-03 18:03:40 +0300

    Using Node as an HTTP client

---
+++ chapter2/fetch.js
@@ -0,0 +1,19 @@
+"use strict";
+const http = require('http');
+const url = require('url');
+let urlOpts = {host: 'www.chebit24.ru', path: '/', port: '80'};
+
+if (process.argv[2]) {
+  if (!process.argv[2].match('http://')) {
+    process.argv[2] = 'http://' + process.argv[2];
+  }
+  urlOpts = url.parse(process.argv[2]);
+}
+
+http.get(urlOpts, (res) => {
+  res.on('data', (chunk) => {
+    console.log(chunk.toString());
+  }).on('error', (e) => {
+    console.log(`error: ${e.message}`);
+  });
+});
