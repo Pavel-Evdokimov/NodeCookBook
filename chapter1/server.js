bc7da0e08ae3ffb447310f6eacbe2fe581678e6b
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2016-02-03 11:21:02 +0300

    Optimizing perfomance with streaming

--- chapter1/server.js
+++ chapter1/server.js
@@ -11,33 +11,12 @@ const mimeTypes = {
 };
 
 let cache = {};
-function cacheAndDeliver(f, cb) {
-  fs.stat(f, (err, stats) => {
-    if (err) {
-      return console.log(`Oh no!, Error`, err);
-    }
-    let lastChanged = Date.parse(stats.ctime);
-    let isUpdated = (cache[f]) && lastChanged > cache[f].timestamp;
-    if (!cache[f] || isUpdated) {
-      fs.readFile(f, (err, data) => {
-        console.log(`loading ${f} from file`);
-        if (!err) {
-          cache[f] = {content: data, timestamp: Date.now()};
-        }
-        cb(err, data);
-      });
-      return;
-    }
-    console.log(`loading ${f} from cache`);
-    cb(null, cache[f].content);
-  });
-}
 
 const server = http.createServer();
 
 server.on('request', (req, res) => {
   let lookup = path.basename(decodeURI(req.url)) || 'index.html';
-  var f = `chapter1/content/${lookup}`;
+  let f = `chapter1/content/${lookup}`;
   if (req.url === '/favicon.ico') {
     console.log(`Not found: ${f}`);
     res.end();
@@ -45,16 +24,32 @@ server.on('request', (req, res) => {
   }
   fs.access(f, fs.R_OK, (err) => {
     if (!err) {
-      cacheAndDeliver(f, (err, data) => {
-        if (err) {
-          res.writeHead(500);
-          res.end('Server Error!');
-          return;
-        }
-        let headers = {'Content-type': mimeTypes[path.extname(f)]};
+      let headers = {'Content-type': mimeTypes[path.extname(f)]};
+      if (cache[f]) {
         res.writeHead(200, headers);
-        res.end(data);
+        res.end(cache[f].content);
+        return;
+      }
+      let s = fs.createReadStream(f);
+      let ws = fs.createWriteStream(`chapter1/content/index2.html`);
+      s.once('open', () => {
+        res.writeHead(200, headers);
+      }).once('error', (e) => {
+        console.log(e);
+        res.writeHead(500);
+        res.end('Server Error!');
       });
+      s.on('data', (chunk) => {
+      });
+      fs.stat(f, (err, stats) => {
+        let bufferOffset = 0;
+        cache[f] = {content: new Buffer(stats.size)};
+        s.on('data', (chunk) => {
+          chunk.copy(cache[f].content, bufferOffset);
+          bufferOffset += chunk.length;
+        });
+      });
+      s.pipe(res);
       return;
     }
     res.writeHead(404);
